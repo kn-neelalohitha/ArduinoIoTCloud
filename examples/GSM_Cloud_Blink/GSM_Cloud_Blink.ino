7f020cb401622ba7630adc4f90300120dcfa48a4
Alexander Entinger <cto@lxrobotics.com>
2019-04-23 06:37:46 +0200

    Simplifying getting started examples by extracting the arduino_secrets into separate file.

--- examples/GSM_Cloud_Blink/GSM_Cloud_Blink.ino
+++ examples/GSM_Cloud_Blink/GSM_Cloud_Blink.ino
@@ -2,25 +2,7 @@
 #include <ConnectionManager.h>
 #include <GSMConnectionManager.h>
 
-#ifndef SECRET_PIN
-  #define SECRET_PIN ""
-  #warning "You need to define SECRET_PIN in tab/arduino_secrets.h"
-#endif
-
-#ifndef SECRET_APN
-  #define SECRET_APN ""
-  #warning "You need to define SECRET_PIN in tab/arduino_secrets.h"
-#endif
-
-#ifndef SECRET_USER_NAME
-  #define SECRET_USER_NAME ""
-  #warning "You need to define SECRET_USER_NAME in tab/arduino_secrets.h"
-#endif
-
-#ifndef SECRET_PASSWORD
-  #define SECRET_PASSWORD ""
-  #warning "You need to define SECRET_PASSWORD in tab/arduino_secrets.h"
-#endif
+#include "arduino_secrets.h"
 
 String cloudSerialBuffer = ""; // the string used to compose network messages from the received characters
 // handles connection to the network
